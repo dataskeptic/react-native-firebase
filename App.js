7393f40e006befa24fd18d83b6987ae1f87f8dea
mathenrifs <mateus_henrique2011@outlook.com>
2021-03-30 12:24:43 -0300

    onboarding

--- App.js
+++ App.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import   { createStackNavigator } from '@react-navigation/stack';
 
 import OnboardingScreen from './screens/OnboardingScreen';
 import LoginScreen from './screens/LoginScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-//import Onboarding from 'react-native-onboarding-swiper';
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator();  
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator      
-        headerMode="none"
-      >
-          <Stack.Screen name="Onboarding" component={OnboardingScreen}/>
-          <Stack.Screen name="Login" component={LoginScreen}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+
+  const [isFirstLaunch, setisFirstLaunch] = React.useState(null);
+  
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if(value == null){
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setisFirstLaunch(true);
+      } else {
+        setisFirstLaunch(false);
+      }
+    });
+  }, []);
+
+  if( isFirstLaunch == null) {
+    return null;
+  } else if ( isFirstLaunch == true) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator      
+          headerMode="none"
+        >
+            <Stack.Screen name="Onboarding" component={OnboardingScreen}/>
+            <Stack.Screen name="Login" component={LoginScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );    
+  } else {
+     return <LoginScreen />
+  }
+
 }
 export default App;
--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,25 +1,68 @@
-import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React from 'react';
+import { TouchableOpacity , Image, Text } from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
+
+const Skip = () => (
+    <TouchableOpacity
+        style={{marginHorizontal: 10}}
+    >
+        <Text style={{fontSize: 16}}>Skip</Text>
+    </TouchableOpacity>
+);
+
+/* const Dots = () => {
+    let backgroundColor;
+    backgroundColor = 'rgba(0, 0, 0, 0.8)';
+
+    return(
+        <View style={{
+            width: 5,
+            heigth: 5,
+            marginHorizontal: 3,
+            backgroundColor,
+        }}>     
+        </View>
+    );
+} */
 
 const OnboardingScreen = ({navigation}) => {
     return (
-        <View style={styles.container}>
-            <Text>Onboarding Screen</Text>
-            <Button 
-                title="Click Here"
-                onPress={() => navigation.navigate("Login")}
-            />
-        </View>
+        <Onboarding
+            SkipButtonComponent={Skip}
+//          DotComponent={Dots}
+            onSkip={() => navigation.replace("Login")}
+            onDone={() => navigation.navigate("Login")}
+            pages={[
+                {
+                    backgroundColor: '#a6e4d0',
+                    image: <Image source={require('../assets/onboarding-img1.png')} />,
+                    title: 'Connect to the world',
+                    subtitle: 'A new way to connect wiht the world',
+                },
+                {
+                    backgroundColor: '#fdeb93',
+                    image: <Image source={require('../assets/onboarding-img2.png')} />,
+                    title: 'Share your favorites',
+                    subtitle: 'Share your thougths with similar kind of peaple',
+                },
+                {
+                    backgroundColor: '#e9bcbe',
+                    image: <Image source={require('../assets/onboarding-img3.png')} />,
+                    title: 'Become the star',
+                    subtitle: 'Let the spot light capture you',
+                },
+  ]}
+/>
     );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
     container: {
         backgroundColor: 'white',
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
     }
-});
+}); */
 
 export default OnboardingScreen;
